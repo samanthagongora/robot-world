174a287cd3e68b316eab82498920747885f3cd15
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-27 17:06:11 -0600

    adds edit function

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,6 +2,7 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get "/robots" do
     @robots = Robot.all
@@ -15,6 +16,16 @@ class RobotWorldApp < Sinatra::Base
   get "/robots/:id" do |id|
     @robot = Robot.find(id)
     erb :show
+  end
+
+  get "/robots/:id/edit" do |id|
+    @robot = Robot.find(id)
+    erb :edit
+  end
+
+  put "/robots/:id" do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
   end
 
   post "/robots" do
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -33,6 +33,21 @@ class Robot
     Robot.new(robot)
   end
 
+  def self.update(id, robot_params)
+    database.execute("UPDATE robots
+                      SET name =?,
+                          city = ?,
+                          state = ?,
+                          department = ?
+                      WHERE id = ?;",
+                      robot_params[:name],
+                      robot_params[:city],
+                      robot_params[:state],
+                      robot_params[:department],
+                      id)
+    Robot.find(id)
+  end
+
   def self.database
     database = SQLite3::Database.new('db/robot_world_development.db')
     database.results_as_hash = true
