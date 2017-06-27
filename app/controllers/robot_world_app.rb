d6675024c1d6125b9eee69972060185afa25cf0a
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-27 15:57:49 -0600

    adds new_robot form and submit route

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,13 +3,23 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
-  get '/robots' do
+  get "/robots" do
     @robots = Robot.all
     erb :index
+  end
+
+  get "/robots/new_robot" do
+    erb :new_robot
   end
 
   get "/robots/:id" do |id|
     @robot = Robot.find(id)
     erb :show
+  end
+
+  post "/robots" do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect "/robots"
   end
 end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,13 +1,24 @@
 require 'sqlite3'
 
 class Robot
-  attr_reader :name, :id
+  attr_reader :id,
+              :name,
+              :city,
+              :state,
+              :department
 
   def initialize(robot_params)
-    @id       = robot_params["id"] if robot_params["id"]
-    @name     = robot_params["name"]
-    @database = SQLite3::Database.new('db/robot_world_development.db')
+    @id         = robot_params["id"] if robot_params["id"]
+    @name       = robot_params["name"]
+    @city       = robot_params["city"]
+    @state      = robot_params["state"]
+    @department = robot_params["department"]
+    @database   = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?);", @name, @city, @state, @department)
   end
 
   def self.all
