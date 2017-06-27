144915df6ddd54b00f72e873cbad47667a6689cd
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-27 15:15:33 -0600

    adds seeded database and all robots view

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -4,7 +4,12 @@ class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
-end
 
+  get "/robots/:id" do
+    @robot = Robot.find(:id)
+    erb :show
+  end
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,29 @@
 require 'sqlite3'
 
 class Robot
+  attr_reader :name
+
   def initialize(robot_params)
+    @name     = robot_params["name"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+  end
+
+  def self.all
+    robots = database.execute("SELECT * FROM robots")
+    robots.map do |robot|
+      Robot.new(robot)
+    end
+  end
+
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id = ?", id).first
+    Robot.new(robot)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
   end
 end
